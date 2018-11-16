573916915cf3f6c670773326a2881368a2241ad0
Daniel Ferrari <danielferrari@live.com>
2018-11-16 15:07:17 -0200

    first commit

--- starter_code/app.js
+++ starter_code/app.js
@@ -9,6 +9,14 @@ const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
 
+// ... inside of app.js
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+
+const index = require('./routes/index');
+const authRoutes = require('./routes/auth');
+
+const laundryRoutes = require('./routes/laundry');
 
 mongoose.Promise = Promise;
 mongoose
@@ -30,6 +38,28 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+app.use(session({
+  secret: 'never do your own laundry again',
+  resave: true,
+  saveUninitialized: true,
+  cookie: { maxAge: 60000 },
+  store: new MongoStore({
+    mongooseConnection: mongoose.connection,
+    ttl: 24 * 60 * 60 // 1 day
+  })
+}));
+
+app.use((req, res, next) => {
+  if (req.session.currentUser) {
+    res.locals.currentUserInfo = req.session.currentUser;
+    res.locals.isUserLoggedIn = true;
+  } else {
+    res.locals.isUserLoggedIn = false;
+  }
+
+  next();
+});
+
 // Express View engine setup
 
 app.use(require('node-sass-middleware')({
@@ -37,22 +67,17 @@ app.use(require('node-sass-middleware')({
   dest: path.join(__dirname, 'public'),
   sourceMap: true
 }));
-      
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
-
-
 // default value for title local
 app.locals.title = 'Express - Generated with IronGenerator';
 
-
-
-const index = require('./routes/index');
 app.use('/', index);
-
+app.use('/', authRoutes);
+app.use('/', laundryRoutes);
 
 module.exports = app;
